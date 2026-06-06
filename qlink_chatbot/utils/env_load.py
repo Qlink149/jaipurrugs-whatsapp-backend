b152449b99484a70ad700af33dd39422410c5eb0
Sidhi Grover <sidhigroverg@gmail.com>
2026-06-06 14:17:21 +0530

    Revert "Support fallback Gupshup env names"
    This reverts commit 726ffa4eafec789ce75ed223cc1026d016b9f0ff.

--- qlink_chatbot/utils/env_load.py
+++ qlink_chatbot/utils/env_load.py
@@ -11,16 +11,11 @@ gupshup_source = os.getenv("GUPSHUP_SOURCE", "")
 default_country_code = os.getenv("DEFAULT_COUNTRY_CODE", "91")
 
 # Qliink / Kisna WhatsApp App
-qlink_gupshup_app_id = os.getenv("QLINK_GUPSHUP_APP_ID") or os.getenv("GUPSHUP_APP_ID", "")
-qlink_gupshup_api_key = os.getenv("QLINK_GUPSHUP_API_KEY") or os.getenv("GUPSHUP_API_KEY", "")
-qlink_gupshup_partner_app_token = (
-    os.getenv("QLINK_GUPSHUP_PARTNER_APP_TOKEN")
-    or os.getenv("GUPSHUP_PARTNER_APP_TOKEN")
-    or os.getenv("GUPSHUP_PARTNER_TOKEN")
-    or os.getenv("GUPSHUP_TOKEN", "")
-)
-qlink_gupshup_app_name = os.getenv("QLINK_GUPSHUP_APP_NAME") or os.getenv("GUPSHUP_APP_NAME", "")
-qlink_gupshup_source = os.getenv("QLINK_GUPSHUP_SOURCE") or os.getenv("GUPSHUP_SOURCE", "")
+qlink_gupshup_app_id = os.getenv("QLINK_GUPSHUP_APP_ID", "")
+qlink_gupshup_api_key = os.getenv("QLINK_GUPSHUP_API_KEY", "")
+qlink_gupshup_partner_app_token = os.getenv("QLINK_GUPSHUP_PARTNER_APP_TOKEN", "")
+qlink_gupshup_app_name = os.getenv("QLINK_GUPSHUP_APP_NAME", "")
+qlink_gupshup_source = os.getenv("QLINK_GUPSHUP_SOURCE", "")
 gupshup_product_template_name = os.getenv(
     "GUPSHUP_PRODUCT_TEMPLATE_NAME", "jaipur_rugs_product_cta"
 )
